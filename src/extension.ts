fd25b37ea61080fad2867165b99497b86c2e09b0
Rhys Howell <rhys@Rhyss-MacBook-Pro.local>
2020-01-31 11:00:35 +0100

    Spaces to tabs in some files and add vs code and tslint rules

--- src/extension.ts
+++ src/extension.ts
@@ -11,18 +11,18 @@ import { mdb } from './mdb';
 // Called when our extension is activated.
 // See "activationEvents" in `package.json` for the events that cause activation.
 export function activate(context: vscode.ExtensionContext) {
-    createExplorerView(context);
+  createExplorerView(context);
 
-    // Register our extension's commands. These are the event handlers and control
-    // the functionality of our extension.
-    registerCommands(context);
+  // Register our extension's commands. These are the event handlers and control
+  // the functionality of our extension.
+  registerCommands(context);
 
-    // TODO: Connect to any existing database connections.
+  // TODO: Resume the context we were in when we last closed (re-open live connections).
 
-    console.log('Congratulations, your extension "mongodb" is now active!');
+  console.log('Congratulations, your extension "mongodb" is now active!');
 }
 
 // Called when our extension is deactivated.
 export function deactivate() {
-    // TODO: Close all active connections & end active queries/playgrounds.
+  // TODO: Close all active connections & end active queries/playgrounds.
 }
--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -7,52 +7,52 @@ import { registerCommands, launchMongoShell } from '../../commands';
 import { TestExtensionContext } from './stubs';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Starting tests...');
+  vscode.window.showInformationMessage('Starting tests...');
 
-	let disposables: vscode.Disposable[] = [];
+  let disposables: vscode.Disposable[] = [];
 
-	teardown(() => {
-		disposables.forEach(d => d.dispose());
-		disposables.length = 0;
-	});
+  teardown(() => {
+    disposables.forEach(d => d.dispose());
+    disposables.length = 0;
+  });
 
-	test('commands are registered in vscode', done => {
-		const mockExtensionContext = new TestExtensionContext();
+  test('commands are registered in vscode', done => {
+    const mockExtensionContext = new TestExtensionContext();
 
-		registerCommands(mockExtensionContext);
+    registerCommands(mockExtensionContext);
 
-		vscode.commands.getCommands().then(registeredCommands => {
-			const expectedCommands = [
-				'mdb.connect',
-				'mdb.addConnection',
-				'mdb.connectWithURI',
-				'mdb.addConnectionWithURI',
-				'mdb.removeConnection',
-				'mdb.launchShell'
-			];
+    vscode.commands.getCommands().then(registeredCommands => {
+      const expectedCommands = [
+        'mdb.connect',
+        'mdb.addConnection',
+        'mdb.connectWithURI',
+        'mdb.addConnectionWithURI',
+        'mdb.removeConnection',
+        'mdb.launchShell'
+      ];
 
-			for (let i = 0; i < expectedCommands.length; i++) {
-				try {
-					assert.notEqual(
-						registeredCommands.indexOf(expectedCommands[i]),
-						-1,
-						`command ${expectedCommands[i]} not registered and was expected`
-					);
-				} catch (e) {
-					done(e);
-					return;
-				}
-			}
+      for (let i = 0; i < expectedCommands.length; i++) {
+        try {
+          assert.notEqual(
+            registeredCommands.indexOf(expectedCommands[i]),
+            -1,
+            `command ${expectedCommands[i]} not registered and was expected`
+          );
+        } catch (e) {
+          done(e);
+          return;
+        }
+      }
 
-			done();
-		});
-	});
+      done();
+    });
+  });
 
-	test('launchMongoShell should open a terminal', done => {
-		disposables.push(vscode.window.onDidOpenTerminal(() => {
-			done();
-		}));
+  test('launchMongoShell should open a terminal', done => {
+    disposables.push(vscode.window.onDidOpenTerminal(() => {
+      done();
+    }));
 
-		launchMongoShell();
-	});
+    launchMongoShell();
+  });
 });
