ce019121018501859cc3dbdf003b10c897bcee9c
Lucas Hrabovsky <hrabovsky.lucas@gmail.com>
2020-03-10 13:04:48 -0400

    feat(logging): Redirect mongodb-js modules using debug to extension logs
    Since we added debug to fix something in the previous commit, we can also get more logging out of data-service, connection-model and friends by [redirecting `debug`'s output stream](https://github.com/visionmedia/debug#output-streams) to the extensions log output channel.

--- src/extension.ts
+++ src/extension.ts
@@ -3,12 +3,19 @@
 import * as vscode from 'vscode';
 
 import { createLogger } from './logging';
+const log = createLogger('extension.ts');
+
+/**
+ * Capture debug logs from mongodb-data-service, connection-model,
+ * etc. and write them to the extension's log output channel too.
+ */
+import debug from 'debug';
+debug.enable('*');
+debug.log = log.debug.bind(log);
 
 import MDBExtensionController from './mdbExtensionController';
 
 let mdbExtension: MDBExtensionController;
-
-const log = createLogger('commands');
 
 // Called when our extension is activated.
 // See "activationEvents" in `package.json` for the events that cause activation.
