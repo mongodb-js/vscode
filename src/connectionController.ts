6fe1ec94f108f173b41ee6aa37f7eaa895c20279
Rhys Howell <rhys@rhysh@live.com>
2020-03-19 11:39:20 +0100

    Tweak message on connect dialogue web view

--- src/connectionController.ts
+++ src/connectionController.ts
@@ -26,7 +26,9 @@ function getConnectWebviewContent(): string {
     </head>
     <body>
       <h1>MongoDB Connection Details Form</h1>
-      <h4>Coming soon :)</h4>
+      <h4>Coming soon.</h4>
+      <h4>For now, to connect please use the command palette (Command+Shift+P) and search 'Connect with Connection String' and enter a connection string.</h4>
+      <h4>Alternatively, you can connect by right clicking the 'Connections' item in the explorer and selecting 'Add MongoDB connection with Connection String...'</h4>
     </body>
   </html>`;
 }
