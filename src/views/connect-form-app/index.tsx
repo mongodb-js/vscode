f9fcedade9ae9d8c0d726a3c08286261c37c08a7
Rhys Howell <rhys@rhysh@live.com>
2020-03-23 22:17:33 +0100

    Add welcome screen and use vscode variables in styling

--- src/views/connect-form-app/index.tsx
+++ src/views/connect-form-app/index.tsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Welcome from './welcome';
 
 import './index.css';
 
 ReactDOM.render(
-  <h1>Hello</h1>,
+  <Welcome />,
   document.getElementById('root')
 );
---
+++ src/views/connect-form-app/welcome.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+export default class Welcome extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>MongoDB Connection Details Form</h1>
+        <h4>Coming soon.</h4>
+        <h4>For now, to connect please use the command palette (Command+Shift+P) and search 'Connect with Connection String' and enter a connection string.</h4>
+        <h4>Alternatively, you can connect by right clicking the 'Connections' item in the explorer and selecting 'Add MongoDB connection with Connection String...'</h4>
+      </div>
+    );
+  }
+}
