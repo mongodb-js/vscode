1b6f4be05ccc459f184256f48b21b43b17868831
Rhys Howell <rhys@rhysh@live.com>
2020-03-24 04:39:37 +0100

    Better styling and react linting

--- src/views/connect-form-app/index.tsx
+++ src/views/connect-form-app/index.tsx
@@ -4,4 +4,7 @@ import Welcome from './welcome';
 
 import './index.css';
 
-ReactDOM.render(<Welcome />, document.getElementById('root'));
+ReactDOM.render(
+  <Welcome />,
+  document.getElementById('root')
+);
