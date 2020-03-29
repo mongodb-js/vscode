9a6751395325304887fa8e551c7ef422aa3d9362
Rhys Howell <rhys@rhysh@live.com>
2020-03-30 01:00:40 +0200

    Remove tabs from connect form

--- src/views/connect-form-app/components/connect-form/connection-form.tsx
+++ src/views/connect-form-app/components/connect-form/connection-form.tsx
@@ -24,16 +24,9 @@ type props = {
   isValid: boolean;
   syntaxErrorMessage: string;
 };
-type state = { activeTab: number };
 
-class ConnectionForm extends React.Component<props, state> {
+class ConnectionForm extends React.Component<props> {
   static displayName = 'ConnectionForm';
-
-  constructor(props) {
-    super(props);
-    this.state = { activeTab: 0 };
-    this.tabs = ['Hostname', 'More Options'];
-  }
 
   /**
    * Resests URL validation if form was changed.
@@ -41,24 +34,6 @@ class ConnectionForm extends React.Component<props, state> {
   onConnectionFormChanged(): void {
     Actions.onConnectionFormChanged();
   }
-
-  /**
-   * Handles the tab click.
-   *
-   * @param {Number} activeTab - The index of the clicked tab.
-   * @param {Object} evt - evt.
-   */
-  onTabClicked(activeTab, evt): void {
-    evt.preventDefault();
-
-    if (this.state.activeTab === activeTab) {
-      return;
-    }
-
-    this.setState({ activeTab });
-  }
-
-  tabs: string[];
 
   /**
    * Renders a port input.
@@ -86,43 +61,7 @@ class ConnectionForm extends React.Component<props, state> {
     }
   }
 
-  /**
-   * Renders tabs.
-   *
-   * @returns {React.Component}
-   */
-  renderTabs(): React.ReactNode {
-    return (
-      <div className={classnames(styles['tabs-header'])}>
-        <ul className={classnames(styles['tabs-header-items'])}>
-          {this.tabs.map((name, key) => {
-            const liClassName = classnames({
-              [styles['tabs-header-item']]: true,
-              [styles['selected-header-item']]: (this.state.activeTab === key)
-            });
-            const spanClassName = classnames({
-              [styles['tabs-header-item-name']]: true,
-              [styles['selected-header-item-name']]: (this.state.activeTab === key)
-            });
-
-            return (
-              <li
-                id={name.replace(/ /g, '_')}
-                key={`tab-${name}`}
-                data-test-id={`${name.toLowerCase().replace(/ /g, '-')}-tab`}
-                onClick={this.onTabClicked.bind(this, key)}
-                className={liClassName}
-              >
-                <span className={spanClassName}>{name}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    );
-  }
-
-  renderHostnameTab(): React.ReactNode {
+  renderHostnameArea(): React.ReactNode {
     const {
       currentConnection,
       isHostChanged,
@@ -135,31 +74,25 @@ class ConnectionForm extends React.Component<props, state> {
       isSrvRecord
     } = currentConnection;
 
-    console.log('renderHostnameTab', hostname);
-
     return (
-      <div className={classnames(styles['tabs-view'])}>
-        <div className={classnames(styles['tabs-view-content'])}>
-          <div className={classnames(styles['tabs-view-content-form'])}>
-            <FormGroup id="connection-host-information" separator >
-              <HostInput
-                hostname={hostname}
-                isHostChanged={isHostChanged}
-              />
-              {this.renderPort()}
-              <SRVInput isSrvRecord={isSrvRecord} />
-            </FormGroup>
-            <Authentication
-              authStrategy={authStrategy}
-              isValid={isValid}
-            />
-          </div>
-        </div>
+      <div>
+        <FormGroup id="connection-host-information" separator >
+          <HostInput
+            hostname={hostname}
+            isHostChanged={isHostChanged}
+          />
+          {this.renderPort()}
+          <SRVInput isSrvRecord={isSrvRecord} />
+        </FormGroup>
+        <Authentication
+          authStrategy={authStrategy}
+          isValid={isValid}
+        />
       </div>
     );
   }
 
-  renderConnectionOptionsTab(): React.ReactNode {
+  renderConnectionOptionsArea(): React.ReactNode {
     const {
       currentConnection
     } = this.props;
@@ -172,26 +105,22 @@ class ConnectionForm extends React.Component<props, state> {
     } = currentConnection;
 
     return (
-      <div className={classnames(styles['tabs-view'])}>
-        <div className={classnames(styles['tabs-view-content'])}>
-          <div className={classnames(styles['tabs-view-content-form'])}>
-            <FormGroup id="read-preference" separator>
-              <ReplicaSetInput
-                sshTunnel={sshTunnel}
-                replicaSet={replicaSet}
-              />
-              <ReadPreferenceSelect
-                readPreference={readPreference}
-              />
-            </FormGroup>
-            <SSLMethod
-              sslMethod={sslMethod}
-            />
-            <SSHTunnel
-              sshTunnel={sshTunnel}
-            />
-          </div>
-        </div>
+      <div>
+        <FormGroup id="read-preference" separator>
+          <ReplicaSetInput
+            sshTunnel={sshTunnel}
+            replicaSet={replicaSet}
+          />
+          <ReadPreferenceSelect
+            readPreference={readPreference}
+          />
+        </FormGroup>
+        <SSLMethod
+          sslMethod={sslMethod}
+        />
+        <SSHTunnel
+          sshTunnel={sshTunnel}
+        />
       </div>
     );
   }
@@ -202,13 +131,12 @@ class ConnectionForm extends React.Component<props, state> {
    * @returns {React.Component}
    */
   renderView(): React.ReactNode {
-    if (this.state.activeTab === 0) {
-      return this.renderHostnameTab();
-    }
-
-    if (this.state.activeTab === 1) {
-      return this.renderConnectionOptionsTab();
-    }
+    return (
+      <div className={styles['fields-view']}>
+        {this.renderHostnameArea()}
+        {this.renderConnectionOptionsArea()}
+      </div>
+    );
   }
 
   render(): React.ReactNode {
@@ -225,9 +153,8 @@ class ConnectionForm extends React.Component<props, state> {
         onChange={this.onConnectionFormChanged.bind(this)}
         className={classnames(styles['connect-form'])}
       >
-        <div className={classnames(styles.tabs)}>
-          <div className={classnames(styles['tabs-container'])}>
-            {this.renderTabs()}
+        <div className={classnames(styles.fields)}>
+          <div className={classnames(styles['fields-container'])}>
             {this.renderView()}
           </div>
         </div>
