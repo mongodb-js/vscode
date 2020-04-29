49abf042f5f90fb0a31be122e649bd7afe0caf75
Rhys Howell <rhys@rhysh@live.com>
2020-04-29 02:41:55 +0200

    add keytar to externals, update default connection saving settings

--- src/utils/getNodeModule.ts
+++ src/utils/getNodeModule.ts
@@ -1,17 +1,14 @@
 import * as vscode from 'vscode';
 
-declare const webpackRequire: typeof require;
-declare const nonWebpackRequire: typeof require;
 export function getNodeModule<T>(moduleName: string): T | undefined {
-  const r = typeof webpackRequire === 'function' ? nonWebpackRequire : require;
   try {
-    return r(`${vscode.env.appRoot}/node_modules.asar/${moduleName}`);
+    return require(`${vscode.env.appRoot}/node_modules.asar/${moduleName}`);
   } catch (err) {
     // Not in ASAR.
   }
 
   try {
-    return r(`${vscode.env.appRoot}/node_modules/${moduleName}`);
+    return require(`${vscode.env.appRoot}/node_modules/${moduleName}`);
   } catch (err) {
     // Not available.
   }
