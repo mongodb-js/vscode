92f7f6c143ae7a038b13d66e3fb00c9865e04cbd
Rhys Howell <rhys@rhysh@live.com>
2020-04-30 15:28:27 +0200

    Clean up credentials after running tests

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -2,6 +2,7 @@ import * as Mocha from 'mocha';
 import * as glob from 'glob';
 import * as vscode from 'vscode';
 import path = require('path');
+import * as keytarType from 'keytar';
 
 import MDBExtensionController from '../../mdbExtensionController';
 import { TestExtensionContext } from './stubs';
@@ -40,13 +41,27 @@ export function run(): Promise<void> {
       vscode.workspace.getConfiguration('mdb.connectionSaving').update(
         'hideOptionToChooseWhereToSaveNewConnections',
         true
-      ).then(() => {
+      ).then(async () => {
+        // We require keytar in runtime because it is a vscode provided
+        // native node module.
+        const keytar: typeof keytarType = require('keytar');
+        const existingCredentials = await keytar.findCredentials('vscode.mongoDB.savedConnections');
+
         // Add files to the test suite.
         files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
-
         try {
           // Run the mocha test.
-          mocha.run(failures => {
+          mocha.run(async (failures) => {
+            // After tests are run we clear any passwords added
+            // to local secure storage.
+            const postRunCredentials = await keytar.findCredentials('vscode.mongoDB.savedConnections');
+            postRunCredentials.forEach(credential => {
+              if (!existingCredentials.find(existingCredential => existingCredential.account === credential.account)) {
+              // If the credential is newly added, we remove it.
+                keytar.deletePassword('vscode.mongoDB.savedConnections', credential.account);
+              }
+            });
+
             if (failures > 0) {
               e(new Error(`${failures} tests failed.`));
             } else {
