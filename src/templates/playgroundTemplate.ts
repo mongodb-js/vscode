05ad1b078d45d5b6d00fc27ac447b0f1c235e08d
Alena Khineika <alenakhineika@users.noreply.github.com>
2020-05-06 09:56:02 +0200

    VSCODE-101: Update a playground template (#71)
    * docs: update a playground template
    
    * feat: use mongodbVSCodePlaygroundDB as test db
    
    * docs: update comment
    
    * docs: update comment
    
    * Minor grammar tweak
    
    Co-authored-by: Massimiliano Marcon <me@marcon.me>

--- src/templates/playgroundTemplate.ts
+++ src/templates/playgroundTemplate.ts
@@ -1,8 +1,14 @@
 const template: string = `// MongoDB Playground
 // To disable this template go to Settings | MongoDB | Use Default Template For Playground.
+// Make sure you are connected to enable completions and to be able to run a playground.
+// Use Ctrl+Space inside a snippet or a string literal to trigger completions.
 
 // Select the database to use.
-use('test');
+use('mongodbVSCodePlaygroundDB');
+
+// The drop() command destroys all data from a collection.
+// Make sure you run it against proper database and collection.
+db.sales.drop();
 
 // Insert a few documents into the sales collection.
 db.sales.insertMany([
