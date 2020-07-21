fcc0199501b2ffa46944166ab90db8ab5c963644
Rhys Howell <rhys@rhysh@live.com>
2020-07-21 18:55:35 +0200

    Add comment for formatting document count

--- src/explorer/documentListTreeItem.ts
+++ src/explorer/documentListTreeItem.ts
@@ -52,6 +52,7 @@ const getCollapsableStateForDocumentList = (
 };
 
 export const formatDocCount = (count: number): string => {
+  // We format the count (30000 -> 30k) and then display it uppercase (30K).
   return `${numeral(count).format('0a')}`.toUpperCase();
 };
 
