7624114aebff0b0e6f6a4dd18ac12e8a73e28bfa
Rhys Howell <rhys@rhysh@live.com>
2020-09-24 16:23:12 +0200

    Update stub with new interface

--- src/test/suite/stubs.ts
+++ src/test/suite/stubs.ts
@@ -18,6 +18,9 @@ class TestExtensionContext implements vscode.ExtensionContext {
   extensionUri;
   environmentVariableCollection;
   extensionMode;
+  storageUri;
+  globalStorageUri;
+  logUri;
 
   asAbsolutePath(relativePath: string): string {
     return relativePath;
@@ -161,7 +164,8 @@ const mockVSCodeTextDocument = {
     undefined,
   validateRange: (range: vscode.Range): vscode.Range => mockRange,
 
-  validatePosition: (position: vscode.Position): vscode.Position => mockPosition
+  validatePosition: (position: vscode.Position): vscode.Position =>
+    mockPosition
 };
 
 class MockLanguageServerController {
