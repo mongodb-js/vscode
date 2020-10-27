bd3dfeee7cc52e68492781171f4ee1dbe260b908
Massimiliano Marcon <max.marcon@mongodb.com>
2020-10-27 10:47:59 +0100

    Atlas sign-in/up uses a localhost redirect (#193)
    * Atlas sign-in/up uses a localhost redirect
    
    This constitutes the prep work to eventually introduce
    a sign-in/sign-up functionality for Atlas. By using a
    short lived local server, we can redirect the user to the
    right Atlas sign-in/up page and wait for an auth redirect
    in VS Code.
    
    * Switched to a proper HTTP redirect and fixed test
    
    Tests are much cleaner now that they use stubs that are restored
    at the end of each test instead of replacing the HTTP and VS Code
    APIs.
    
    * Removed unnecessary comma
    
    * Wrap link opener in try catch, defaulting to regular link opening on error
    
    * Error handling logic
    
    Co-authored-by: Anemy <rhys@rhysh@live.com>

---
+++ src/test/suite/utils/linkHelper.test.ts
@@ -0,0 +1,46 @@
+import { openLink } from '../../../utils/linkHelper';
+import { expect } from 'chai';
+import vscode from 'vscode';
+import http from 'http';
+import { EventEmitter } from 'events';
+const sinon = require('sinon');
+
+suite('Open Link Test Suite', () => {
+  test('the helper server is instantiated correctly', () => {
+    const stubServer = { on: sinon.spy(), listen: sinon.spy() };
+    const stubCreateServer = sinon.stub(http, 'createServer').returns(stubServer);
+    openLink('https://mongodb.com', 4321);
+    expect(stubServer.on.calledWith('connection')).to.be.true;
+    expect(stubServer.listen.calledWith(4321)).to.be.true;
+    stubCreateServer.restore();
+  });
+
+  test('the browser opens correctly', () => {
+    const stubServer = sinon.createStubInstance(http.Server, {
+      on: sinon.stub(),
+      listen: sinon.stub().callsArg(1)
+    });
+    const stubCreateServer = sinon.stub(http, 'createServer').returns(stubServer);
+    const stubExecuteCommand = sinon.stub(vscode.commands, 'executeCommand').resolves();
+    openLink('https://mongodb.com', 4321);
+    expect(stubExecuteCommand.firstCall.args[0]).to.equal('vscode.open');
+    expect(stubExecuteCommand.firstCall.args[1].path).to.equal(vscode.Uri.parse('https://mongodb.com:4322').path);
+    stubExecuteCommand.restore();
+    stubCreateServer.restore();
+  });
+
+  test('handles errors', (done) => {
+    class MockedServer extends EventEmitter {
+      listen() { }
+    }
+    const mockedServer = new MockedServer();
+    const stubCreateServer = sinon.stub(http, 'createServer').returns(mockedServer);
+    openLink('https://mongodb.com', 4321).catch((e) => {
+      expect(e.message).to.equal('some error');
+      stubCreateServer.restore();
+      done();
+    });
+    mockedServer.emit('error', new Error('some error'));
+  });
+});
+
