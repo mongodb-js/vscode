3f321c5740e32d31966920eb9526c6281680489b
Anemy <rhysh@live.com>
2020-11-17 00:18:11 -0500

    Add links and resources, stylize

--- src/views/webview-app/components/resources-panel/resources-panel.tsx
+++ src/views/webview-app/components/resources-panel/resources-panel.tsx
@@ -7,16 +7,75 @@ import bookIcon from '@iconify-icons/codicon/book';
 
 import {
   ActionTypes,
+  LinkClickedAction,
   ToggleShowResourcesPanelAction
 } from '../../store/actions';
 
 const styles = require('./resources-panel.less');
 
+const ResourceLinks = [{
+  title: 'Product overview',
+  description: 'Get an overview on MongoDB',
+  linkId: 'productOverview',
+  url: 'https://www.mongodb.com/products/vs-code'
+  // TODO: Should we use a special link.
+}, {
+  // TODO: Do we pair this with the help tree resource links?
+  title: 'Extension documentation',
+  description: 'Check the documentation about the extension',
+  linkId: 'extensionDocumentation',
+  url: 'https://docs.mongodb.com/mongodb-vscode/'
+}, {
+  title: 'Connect to your database',
+  description: 'Connect in just a few steps',
+  url: 'https://docs.mongodb.com/mongodb-vscode/connect',
+  linkId: 'connectInfo'
+}, {
+  title: 'Interact with your data',
+  description: 'Play with your data, create queries and aggregations',
+  // TODO: This link good?
+  url: 'https://docs.mongodb.com/mongodb-vscode/playgrounds',
+  linkId: 'interactWithYourData'
+}];
+
+const FooterFeatures = [{
+  title: 'Navigate databases'
+}, {
+  title: 'Perform CRUD operations'
+}, {
+  title: 'Run aggregation pipelines'
+}, {
+  title: 'Playgrounds'
+}];
+
+const FooterLinks = [{
+  title: 'Github',
+  linkId: 'github',
+  url: 'https://github.com/mongodb-js/vscode'
+}, {
+  title: 'Stack Overflow',
+  linkId: 'stackOverflow',
+  url: 'https://stackoverflow.com/questions'
+}, {
+  title: 'Suggest a feature',
+  linkId: 'feedback',
+  url: 'https://feedback.mongodb.com/forums/929236-mongodb-for-vs-code/'
+}, {
+  title: 'Report a bug',
+  linkId: 'reportABug',
+  url: 'https://github.com/mongodb-js/vscode/issues'
+}];
+
 type DispatchProps = {
+  onLinkClicked: (screen: string, linkId: string) => void;
   toggleShowResourcesPanel: () => void;
 };
 
 class ResourcesPanel extends React.Component<DispatchProps> {
+  onLinkClicked = (screen: string, linkId: string): void => {
+    this.props.onLinkClicked(screen, linkId);
+  };
+
   renderHeader(): React.ReactNode {
     return (
       <React.Fragment>
@@ -38,6 +97,75 @@ class ResourcesPanel extends React.Component<DispatchProps> {
     );
   }
 
+  renderLinks(): React.ReactNode {
+    return (
+      <div className={styles['resources-panel-links-container']}>
+        {ResourceLinks.map(resourceLink => (
+          <a
+            className={styles['resources-panel-link']}
+            href={resourceLink.url}
+            onClick={(): void => this.onLinkClicked(
+              'overviewResourcesPanel',
+              resourceLink.linkId
+            )}
+            key={`link-${resourceLink.linkId}`}
+          >
+            <div>
+              <strong>
+                {resourceLink.title}
+              </strong>
+            </div>
+            <div>
+              {resourceLink.description}
+            </div>
+          </a>
+        ))}
+      </div>
+    );
+  }
+
+  renderFooter(): React.ReactNode {
+    return (
+      <div className={styles['resources-panel-footer']}>
+        <div className={styles['resources-panel-footer-item']}>
+          <div className={styles['resources-panel-footer-item-title']}>
+            <strong>
+              Key Features
+            </strong>
+          </div>
+          {FooterFeatures.map(footerFeature => (
+            <div
+              className={styles['resources-panel-footer-link']}
+              key={`footer-feature-${footerFeature.title}`}
+            >
+              {footerFeature.title}
+            </div>
+          ))}
+        </div>
+        <div className={styles['resources-panel-footer-item']}>
+          <div className={styles['resources-panel-footer-item-title']}>
+            <strong>
+              Contribute
+            </strong>
+          </div>
+          {FooterLinks.map(footerLink => (
+            <a
+              className={styles['resources-panel-footer-link']}
+              href={footerLink.url}
+              onClick={(): void => this.onLinkClicked(
+                'overviewResourcesPanel',
+                footerLink.linkId
+              )}
+              key={`footer-link-${footerLink.linkId}`}
+            >
+              {footerLink.title}
+            </a>
+          ))}
+        </div>
+      </div>
+    );
+  }
+
   render(): React.ReactNode {
     return (
       <div className={styles['resources-panel']}>
@@ -47,6 +175,8 @@ class ResourcesPanel extends React.Component<DispatchProps> {
         />
         <div className={styles['resources-panel-content']}>
           {this.renderHeader()}
+          {this.renderLinks()}
+          {this.renderFooter()}
         </div>
       </div>
     );
@@ -54,6 +184,11 @@ class ResourcesPanel extends React.Component<DispatchProps> {
 }
 
 const mapDispatchToProps: DispatchProps = {
+  onLinkClicked: (screen, linkId): LinkClickedAction => ({
+    type: ActionTypes.EXTENSION_LINK_CLICKED,
+    screen,
+    linkId
+  }),
   toggleShowResourcesPanel: (): ToggleShowResourcesPanelAction => ({
     type: ActionTypes.TOGGLE_SHOW_RESOURCES_PANEL
   })
