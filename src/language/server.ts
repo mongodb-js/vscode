2e6813eb88b01fb3985b8de41a5d164a572ec130
Anna Henningsen <anna.henningsen@mongodb.com>
2021-04-08 13:06:43 +0200

    fix: remove placeholder validation from language server VSCODE-246

--- src/language/server.ts
+++ src/language/server.ts
@@ -1,8 +1,6 @@
 import {
   createConnection,
   TextDocuments,
-  Diagnostic,
-  DiagnosticSeverity,
   ProposedFeatures,
   InitializeParams,
   DidChangeConfigurationNotification,
@@ -31,7 +29,7 @@ const mongoDBService = new MongoDBService(connection);
 
 let hasConfigurationCapability = false;
 // let hasWorkspaceFolderCapability = false;
-let hasDiagnosticRelatedInformationCapability = false;
+// let hasDiagnosticRelatedInformationCapability = false;
 
 connection.onInitialize((params: InitializeParams) => {
   const capabilities = params.capabilities;
@@ -44,11 +42,11 @@ connection.onInitialize((params: InitializeParams) => {
   // hasWorkspaceFolderCapability = !!(
   //   capabilities.workspace && !!capabilities.workspace.workspaceFolders
   // );
-  hasDiagnosticRelatedInformationCapability = !!(
-    capabilities.textDocument &&
-    capabilities.textDocument.publishDiagnostics &&
-    capabilities.textDocument.publishDiagnostics.relatedInformation
-  );
+  // hasDiagnosticRelatedInformationCapability = !!(
+  //  capabilities.textDocument &&
+  //  capabilities.textDocument.publishDiagnostics &&
+  //  capabilities.textDocument.publishDiagnostics.relatedInformation
+  // );
 
   return {
     capabilities: {
@@ -91,47 +89,22 @@ interface ExampleSettings {
 // The global settings, used when the `workspace/configuration` request is not supported by the client.
 // Please note that this is not the case when using this server with the client provided in this example
 // but could happen with other clients.
-const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
-let globalSettings: ExampleSettings = defaultSettings;
+// const defaultSettings: ExampleSettings = { maxNumberOfProblems: 1000 };
+// let globalSettings: ExampleSettings = defaultSettings;
 
 // Cache the settings of all open documents.
 const documentSettings: Map<string, Thenable<ExampleSettings>> = new Map();
 
-connection.onDidChangeConfiguration((change) => {
+connection.onDidChangeConfiguration((/* change */) => {
   if (hasConfigurationCapability) {
     // Reset all cached document settings.
     documentSettings.clear();
   } else {
-    // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
-    globalSettings = <ExampleSettings>(
-      (change.settings.mongodbLanguageServer || defaultSettings)
-    );
+    // globalSettings = <ExampleSettings>(
+    //   (change.settings.mongodbLanguageServer || defaultSettings)
+    // );
   }
-
-  // Revalidate all open text documents.
-  // eslint-disable-next-line @typescript-eslint/no-use-before-define
-  documents.all().forEach((document: TextDocument) => {
-    validateTextDocument(document);
-  });
 });
-
-const getDocumentSettings = (resource: string): Thenable<ExampleSettings> => {
-  if (!hasConfigurationCapability) {
-    return Promise.resolve(globalSettings);
-  }
-
-  let result = documentSettings.get(resource);
-
-  if (!result) {
-    result = connection.workspace.getConfiguration({
-      scopeUri: resource,
-      section: 'mongodbLanguageServer'
-    });
-    documentSettings.set(resource, result);
-  }
-
-  return result;
-};
 
 // Only keep settings for open documents.
 documents.onDidClose((e) => {
@@ -140,79 +113,12 @@ documents.onDidClose((e) => {
   documentSettings.delete(e.document.uri);
 });
 
-const validateTextDocument = async (
-  textDocument: TextDocument
-): Promise<void> => {
-  // In this simple example we get the settings for every validate run.
-  const settings = await getDocumentSettings(textDocument.uri);
-
-  // The validator creates diagnostics for all uppercase words length 2 and more.
-  const text = textDocument.getText();
-  const pattern = /\b[A-Z]{2,}\b/g;
-  const diagnostics: Diagnostic[] = [];
-  let m: RegExpExecArray | null;
-  let problems = 0;
-
-  // eslint-disable-next-line no-cond-assign
-  while ((m = pattern.exec(text)) && problems < settings.maxNumberOfProblems) {
-    problems++;
-
-    const diagnostic: Diagnostic = {
-      severity: DiagnosticSeverity.Warning,
-      range: {
-        start: textDocument.positionAt(m.index),
-        end: textDocument.positionAt(m.index + m[0].length)
-      },
-      message: `${m[0]} is all uppercase.`,
-      source: 'ex'
-    };
-
-    if (hasDiagnosticRelatedInformationCapability) {
-      diagnostic.relatedInformation = [
-        {
-          location: {
-            uri: textDocument.uri,
-            range: Object.assign({}, diagnostic.range)
-          },
-          message: 'Spelling matters'
-        },
-        {
-          location: {
-            uri: textDocument.uri,
-            range: Object.assign({}, diagnostic.range)
-          },
-          message: 'Particularly for names'
-        }
-      ];
-    }
-
-    diagnostics.push(diagnostic);
-  }
-
-  // Send the computed diagnostics to VSCode.
-  // connection.console.log(
-  //   `sendDiagnostics: ${JSON.stringify({ uri: textDocument.uri, diagnostics })}`
-  // );
-
-  connection.sendDiagnostics({ uri: textDocument.uri, diagnostics });
-
-  // const notification = new NotificationType<string>('showInformationMessage');
-  // connection.sendNotification(
-  //   'showInformationMessage',
-  //   'Enjoy these diagnostics!'
-  // );
-};
-
 // The content of a text document has changed. This event is emitted
 // when the text document first opened or when its content has changed.
-documents.onDidChangeContent((change) => {
+documents.onDidChangeContent((/* change */) => {
   // connection.console.log(
   //   `documents.onDidChangeContent: ${JSON.stringify(change)}`
   // );
-
-  if (change.document) {
-    validateTextDocument(change.document);
-  }
 });
 
 connection.onRequest(new RequestType('textDocument/codeLens'), (/* event*/) => {
