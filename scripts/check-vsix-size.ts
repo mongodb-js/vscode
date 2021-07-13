d5a3e3edf0de95ae01087ec34eff76f09cfdb7db
mcasimir <maurizio.cas@gmail.com>
2021-07-13 13:24:30 +0200

    build on macOs

---
+++ scripts/check-vsix-size.ts
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+
+const version = JSON.parse(fs.readFileSync(path.resolve(__dirname, '..', 'package.json')).toString()).version;
+
+const vsixFileName = path.resolve(__dirname, '..', `./mongodb-vscode-${version}.vsix`);
+const size = fs.statSync(vsixFileName).size;
+
+const maxSize = 6 * 1000000; // 6 MB
+
+if (size >= maxSize) {
+  throw new Error(`vsix bundle too big expected max ${maxSize} bytes, got ${size}.`);
+}
+
+console.info(`vsix size ok: ${size} bytes.`);
