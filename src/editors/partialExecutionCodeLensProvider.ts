7bc8c96027c95d3443aed6cbacf07499f84f9e38
Rhys <Anemy@users.noreply.github.com>
2021-07-26 10:11:10 -0400

    feat(playgrounds): Show code lens at end of selection (#324)

--- src/editors/partialExecutionCodeLensProvider.ts
+++ src/editors/partialExecutionCodeLensProvider.ts
@@ -1,6 +1,57 @@
 import * as vscode from 'vscode';
 import EXTENSION_COMMANDS from '../commands';
 
+// Returns a boolean if the selection is one that is valid to show a
+// code lens for (isn't a partial line etc.).
+export function isSelectionValidForCodeLens(
+  selections: vscode.Selection[],
+  textDocument: vscode.TextDocument
+): boolean {
+  if (selections.length > 1) {
+    // Show codelens when it's multi cursor.
+    return true;
+  }
+
+  if (!selections[0].isSingleLine) {
+    // Show codelens when it's a multi-line selection.
+    return true;
+  }
+
+  const lineContent = (textDocument.lineAt(selections[0].end.line).text || '').trim();
+  const selectionContent = (textDocument.getText(selections[0]) || '').trim();
+
+  // Show codelens when it contains the whole line.
+  return lineContent === selectionContent;
+}
+
+export function getCodeLensLineOffsetForSelection(
+  selections: vscode.Selection[],
+  editor: vscode.TextEditor
+): number {
+  const lastSelection = selections[selections.length - 1];
+  const lastSelectedLineNumber = lastSelection.end.line;
+  const lastSelectedLineContent = editor.document.lineAt(lastSelectedLineNumber).text || '';
+
+  // Show a code lens after the selected line, unless the
+  // contents of the selection in the last line is empty.
+  const lastSelectedLineContentRange = new vscode.Range(
+    new vscode.Position(
+      lastSelection.end.line,
+      0
+    ),
+    new vscode.Position(
+      lastSelection.end.line,
+      lastSelectedLineContent.length
+    )
+  );
+  const interectedSelection = lastSelection.intersection(lastSelectedLineContentRange);
+  if (!interectedSelection || interectedSelection.isEmpty) {
+    return 0;
+  }
+
+  return lastSelectedLineContent.trim().length > 0 ? 1 : 0;
+}
+
 export default class PartialExecutionCodeLensProvider
 implements vscode.CodeLensProvider {
   _selection?: vscode.Range;
