3a3ea4fce96fb08e5d5ead04f211351a73616d3b
Alena Khineika <alena.khineika@gmail.com>
2021-08-04 23:00:06 +0200

    refactor: try new and updated seedDataAndCreateDataService

--- src/test/suite/dbTestHelper.ts
+++ src/test/suite/dbTestHelper.ts
@@ -14,62 +14,28 @@ export const TEST_DB_NAME = 'vscodeTestDatabaseAA';
 let testDatabaseConnectionModel;
 
 // Note: Be sure to disconnect from the dataservice to free up connections.
-export const seedDataAndCreateDataService = (
+export const seedDataAndCreateDataService = async (
   collectionName: string,
   documentsArray: any[]
-): Promise<any> => {
-  return new Promise((resolve, reject) => {
-    if (testDatabaseConnectionModel) {
-      const newConnection = new DataService(testDatabaseConnectionModel);
-      newConnection.connect((connectError: Error | undefined) => {
-        if (connectError) {
-          return reject(connectError);
-        }
+): Promise<Connection> => {
+  if (!testDatabaseConnectionModel) {
+    const connectionFrom = promisify(Connection.from.bind(Connection));
 
-        newConnection.insertMany(
-          `${TEST_DB_NAME}.${collectionName}`,
-          documentsArray,
-          {},
-          (insertManyError: Error | undefined) => {
-            if (insertManyError) {
-              return reject(insertManyError);
-            }
-            return resolve(newConnection);
-          }
-        );
-      });
-    } else {
-      Connection.from(
-        TEST_DATABASE_URI,
-        (connectionModelError: Error | undefined, newConnectionConfig: any) => {
-          if (connectionModelError) {
-            return reject(connectionModelError);
-          }
-
-          testDatabaseConnectionModel = newConnectionConfig;
-          const newConnection = new DataService(newConnectionConfig);
-          newConnection.connect((connectError: Error | undefined) => {
-            if (connectError) {
-              return reject(connectError);
-            }
-
-            newConnection.insertMany(
-              `${TEST_DB_NAME}.${collectionName}`,
-              documentsArray,
-              {},
-              (insertManyError: Error | undefined) => {
-                if (insertManyError) {
-                  return reject(insertManyError);
-                }
-
-                return resolve(newConnection);
-              }
-            );
-          });
-        }
-      );
+    try {
+      testDatabaseConnectionModel = await connectionFrom(TEST_DATABASE_URI);
+    } catch (error) {
+      throw new Error(`Error connecting to ${TEST_DATABASE_URI}: ${error}`);
     }
-  });
+  }
+
+  const newConnection = new DataService(testDatabaseConnectionModel);
+  const connect = promisify(newConnection.connect.bind(newConnection));
+  const insertMany = promisify(newConnection.insertMany.bind(newConnection));
+
+  await connect();
+  await insertMany(`${TEST_DB_NAME}.${collectionName}`, documentsArray, {});
+
+  return newConnection;
 };
 
 export const cleanupTestDB = async (): Promise<void> => {
