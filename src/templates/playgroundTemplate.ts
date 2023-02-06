615f04a08ccf4b67b0b88d378126e4654bd117e3
Rhys Howell <rhysh@live.com>
2023-02-06 09:17:51 -0500

    use db.getCollection() format

--- src/templates/playgroundTemplate.ts
+++ src/templates/playgroundTemplate.ts
@@ -10,7 +10,7 @@ const template = `// MongoDB Playground
 use('mongodbVSCodePlaygroundDB');
 
 // Insert a few documents into the sales collection.
-db['sales'].insertMany([
+db.getCollection('sales').insertMany([
   { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
   { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
   { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
@@ -22,7 +22,7 @@ db['sales'].insertMany([
 ]);
 
 // Run a find command to view items sold on April 4th, 2014.
-const salesOnApril4th = db['sales'].find({
+const salesOnApril4th = db.getCollection('sales').find({
   date: { $gte: new Date('2014-04-04'), $lt: new Date('2014-04-05') }
 }).toArray();
 
@@ -32,7 +32,7 @@ console.log(\`\${salesOnApril4th.length} sales occurred in 2014.\`);
 // Here we run an aggregation and open a cursor to the results.
 // Use '.toArray()' to exhaust the cursor to return the whole result set.
 // You can use '.hasNext()/.next()' to iterate through the cursor page by page.
-db['sales'].aggregate([
+db.getCollection('sales').aggregate([
   // Find all of the sales that occurred in 2014.
   { $match: { date: { $gte: new Date('2014-01-01'), $lt: new Date('2015-01-01') } } },
   // Group the total sales for each product.
