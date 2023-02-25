c9f2f79ff2ab1b8f6a774f0adc9b613dc75621f9
Alena Khineika <alena.khineika@gmail.com>
2023-02-25 19:59:02 +0100

    feat: proper js env in the output renderer

--- src/notebook/renderer/index.tsx
+++ src/notebook/renderer/index.tsx
@@ -1,35 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { NotebookOutput } from './render';
+import { render } from './render';
 import errorOverlay from 'vscode-notebook-error-overlay';
 import type { ActivationFunction } from 'vscode-notebook-renderer';
 
 export const activate: ActivationFunction = (context) => {
   return {
     renderOutputItem(outputItem, element) {
-      let shadow = element.shadowRoot;
-      if (!shadow) {
-        shadow = element.attachShadow({ mode: 'open' });
-        const root = document.createElement('div');
-        root.id = 'root';
-        shadow.append(root);
-      }
-
-      const root = shadow.querySelector<HTMLElement>('#root');
-      if (!root) {
-        throw new Error('Could not find root element');
-      }
-
-      errorOverlay.wrap(root, () => {
-        root.innerHTML = '';
-        const node = document.createElement('div');
-        root.appendChild(node);
-
-        ReactDOM.render(
-          <NotebookOutput info={{ container: node, outputItem, context }} />,
-          root
-        );
+      errorOverlay.wrap(element, () => {
+        const cellOutputContainer: HTMLDivElement =
+          document.createElement('div');
+        element.appendChild(cellOutputContainer);
+        render({
+          container: cellOutputContainer,
+          mimeType: outputItem.mime,
+          value: outputItem,
+          context,
+        });
       });
     },
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
--- src/notebook/renderer/render.tsx
+++ src/notebook/renderer/render.tsx
@@ -1,24 +1,105 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import { css } from '@emotion/css';
+
 import type { RendererContext, OutputItem } from 'vscode-notebook-renderer';
 
+/**
+ * Notebook cell output render info.
+ */
 interface IRenderInfo {
   container: HTMLElement;
-  outputItem: OutputItem;
+  mimeType: string;
+  value: OutputItem;
   context: RendererContext<unknown>;
 }
 
-interface NotebookOutputProps {
-  info: IRenderInfo;
+interface NotebookOutputErrorProps {
+  error: Error | { message: string };
 }
 
-export const NotebookOutput = ({ info }: NotebookOutputProps) => {
-  // const json = JSON.parse(info.outputItem.text());
-  const error = info.outputItem.json();
-  // const color = new vscode.ThemeColor('editorError.foreground') || 'while';
+const NotebookOutputError = ({ error }: NotebookOutputErrorProps) => {
+  const [errorClosed, setErrorClosed] = useState(false);
+
+  const onCloseError = () => {
+    setErrorClosed(true);
+  };
+
+  const rootStyles = css({
+    fontSize: '13px',
+    background: 'rgb(90, 29, 29)',
+    border: '1px solid rgb(190, 17, 0)',
+    padding: '5px',
+    margin: '-6px',
+  });
+
+  const titleStyles = css({
+    fontSize: '1.5em',
+    margin: '0px 0px 0.25em',
+    fontWeight: 'normal',
+  });
+
+  const textStyles = css({
+    fontFamily: 'Menlo, Monaco, &quot;Courier New&quot;, monospace',
+    fontSize: '12px',
+    fontWeight: 'normal',
+    width: '100%',
+    overflowX: 'auto',
+    lineHeight: '1.5em',
+    background: 'rgba(0, 0, 0, 0.2)',
+  });
+
+  const errorMessageStyles = css({
+    padding: '0.1em 0.3em',
+    display: 'table-row',
+  });
+
+  const closeErrorButtonStyles = css({
+    position: 'absolute',
+    top: '3px',
+    right: '8px',
+    border: '0px',
+    background: 'none',
+    padding: '0px',
+    margin: '0px',
+    outline: '0px',
+    cursor: 'pointer',
+  });
+
+  if (errorClosed) {
+    return null;
+  }
+
   return (
-    <div>
-      <h1>Failed with error</h1>
-      <p>{error.message}</p>
+    <div className={rootStyles}>
+      <h1 className={titleStyles}>Failed with error:</h1>
+      <pre className={textStyles}>
+        <code
+          className={errorMessageStyles}
+        >{`${error.name}: ${error.message}`}</code>
+        <button
+          onClick={onCloseError}
+          title="Close"
+          className={closeErrorButtonStyles}
+        >
+          <svg
+            width="9"
+            height="9"
+            viewBox="0 0 16 16"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <line x1="16" y1="16" x2="0" y2="1" stroke="white" />
+            <line x1="16" y1="1" x2="1" y2="16" stroke="white" />
+          </svg>
+        </button>
+      </pre>
     </div>
   );
+};
+
+export const render = (output: IRenderInfo) => {
+  const error = output.value.json();
+
+  ReactDOM.render(<NotebookOutputError error={error} />, output.container);
 };
