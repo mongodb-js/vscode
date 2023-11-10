899a15abeb5f1ccc38e4240f4f2390c6ac37b18f
Rhys Howell <rhys.howell@mongodb.com>
2023-11-10 13:24:08 -0500

    chore: update mongodb-runner to start and stop outside package

--- src/test/runTest.ts
+++ src/test/runTest.ts
@@ -1,9 +1,37 @@
 import path from 'path';
 import { runTests } from '@vscode/test-electron';
+import crossSpawn from 'cross-spawn';
+import fs from 'fs';
 
 // More information on vscode specific tests: https://github.com/microsoft/vscode-test
 
+function startTestMongoDBServer() {
+  console.log('Starting MongoDB server');
+  crossSpawn.sync('npm', ['run', 'start-test-server'], { stdio: 'inherit' });
+}
+
+function cleanupTestMongoDBServer() {
+  console.log('Stopping MongoDB server and cleaning up server data');
+  try {
+    crossSpawn.sync('npm', ['run', 'stop-test-server'], {
+      // If it's taking too long we might as well kill the process and
+      // move on, mongodb-runner is flaky sometimes.
+      timeout: 30_000,
+      stdio: 'inherit',
+    });
+  } catch (e) {
+    console.error('Failed to stop MongoDB Server', e);
+  }
+  try {
+    fs.rmdirSync('.mongodb', { recursive: true });
+  } catch (e) {
+    console.error('Failed to clean up server data', e);
+  }
+}
+
 async function main(): Promise<any> {
+  startTestMongoDBServer();
+
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
@@ -26,6 +54,8 @@ async function main(): Promise<any> {
     console.error('Failed to run tests:');
     console.error(err);
     process.exit(1);
+  } finally {
+    cleanupTestMongoDBServer();
   }
 }
 
