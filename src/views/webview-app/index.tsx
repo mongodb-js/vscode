d93f5cf8eebd05467ed4108eda2984df7740a458
Himanshu Singh <himanshu.singhs@outlook.in>
2023-11-24 15:15:37 +0100

    fix: remove store from main index file

--- src/views/webview-app/index.tsx
+++ src/views/webview-app/index.tsx
@@ -1,17 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
 import App from './app';
 
-import { initialState, rootReducer } from './legacy/store/store';
-
-const store = createStore(rootReducer, initialState);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
