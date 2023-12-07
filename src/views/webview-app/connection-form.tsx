708520cfd972b53f0c320cb548a64d80fb56b9e1
Rhys Howell <rhys.howell@mongodb.com>
2023-12-07 11:18:32 -0500

    chore: update connection-form versions, add reset-css

--- src/views/webview-app/connection-form.tsx
+++ src/views/webview-app/connection-form.tsx
@@ -3,6 +3,11 @@ import CompassConnectionForm from '@mongodb-js/connection-form';
 import { Modal, ModalBody, css, spacing } from '@mongodb-js/compass-components';
 import { v4 as uuidv4 } from 'uuid';
 
+const modalContentStyles = css({
+  // Override LeafyGreen width to accommodate the strict connection-form size.
+  width: `${spacing[6] * 12}px !important`,
+});
+
 const formContainerStyles = css({
   padding: spacing[1],
 });
@@ -26,6 +31,9 @@ const ConnectionForm: React.FunctionComponent<{
   return (
     <>
       <Modal
+        // Warning: This property may be removed in future
+        // modal releases.
+        contentClassName={modalContentStyles}
         setOpen={() => onClose()}
         open={open}
         data-testid="connection-form-modal"
--- src/views/webview-app/index.tsx
+++ src/views/webview-app/index.tsx
@@ -2,5 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import App from './app';
+// import './reset.less';
 
+import { resetGlobalCSS } from './reset-css';
+
+resetGlobalCSS();
 ReactDOM.render(<App />, document.getElementById('root'));
