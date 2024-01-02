0df5b84cda2ad26e727a46eff1a43455b77a8e8e
Rhys Howell <rhys.howell@mongodb.com>
2024-01-02 10:47:16 -0500

    update feature flag to always show new connection form

--- src/test/suite/views/webview-app/app.test.tsx
+++ src/test/suite/views/webview-app/app.test.tsx
@@ -7,15 +7,15 @@ import App from '../../../../views/webview-app/app';
 
 describe('App Component Test Suite', () => {
   afterEach(() => sinon.restore());
-  test('it renders the old overview page when useNewConnectionForm is falsy', () => {
+  test('it renders the old overview page when useOldConnectionForm is falsy', () => {
     sinon.stub(featureFlags, 'getFeatureFlag').returns(false);
     render(<App />);
-    expect(() => screen.getAllByTestId('legacy-app')).does.not.throw;
+    expect(screen.queryByTestId('legacy-app')).to.be.null;
   });
 
-  test('it renders the new overview page when useNewConnectionForm is truthy', () => {
+  test('it renders the new overview page when useOldConnectionForm is truthy', () => {
     sinon.stub(featureFlags, 'getFeatureFlag').returns(true);
     render(<App />);
-    expect(screen.queryByTestId('legacy-app')).to.be.null;
+    expect(() => screen.getAllByTestId('legacy-app')).does.not.throw;
   });
 });
