fb974ce63c281f0009b9a117bd129f20d99f4fbf
Alena Khineika <alena.khineika@mongodb.com>
2024-09-16 17:49:47 +0200

    refactor: update messages

--- scripts/generate-constants.ts
+++ scripts/generate-constants.ts
@@ -14,22 +14,18 @@ const ui = ora('Generate constants file').start();
 config({ path: resolve(__dirname, '../.env') });
 
 (async () => {
-  if (process.env.SEGMENT_KEY || process.env.MONGODB_DOCS_CHATBOT_BASE_URI) {
-    await writeFile(
-      `${ROOT_DIR}/constants.json`,
-      JSON.stringify(
-        {
-          segmentKey: process.env.SEGMENT_KEY,
-          docsChatbotBaseUri: process.env.MONGODB_DOCS_CHATBOT_BASE_URI,
-        },
-        null,
-        2
-      )
-    );
-    ui.succeed('Generated segment constants file');
-  } else {
-    throw new Error('The Segment key is missing in environment variables');
-  }
+  await writeFile(
+    `${ROOT_DIR}/constants.json`,
+    JSON.stringify(
+      {
+        segmentKey: process.env.SEGMENT_KEY,
+        docsChatbotBaseUri: process.env.MONGODB_DOCS_CHATBOT_BASE_URI,
+      },
+      null,
+      2
+    )
+  );
+  ui.succeed('The constants file has been generated');
 })().catch((error) => {
-  ui.fail(`Failed to generate segment constants file: ${error.message}`);
+  ui.fail(`Failed to generate constants file: ${error.message}`);
 });
