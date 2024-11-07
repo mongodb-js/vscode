aba650cba8b58c6967f452838f976964714430f6
gagik <me@gagik.co>
2024-11-07 22:37:23 +0100

    delete mistaken duplicate

--- src/test/suite/participant/participantErrorTypes.ts
+++
@@ -1,7 +0,0 @@
-export enum ParticipantErrorTypes {
-  CHAT_MODEL_OFF_TOPIC = 'Chat Model Off Topic',
-  INVALID_PROMPT = 'Invalid Prompt',
-  FILTERED = 'Filtered by Responsible AI Service',
-  OTHER = 'Other',
-  DOCS_CHATBOT_API = 'Docs Chatbot API Issue',
-}
