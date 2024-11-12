834faaed6e4cb56e0bc60cc5d1152c5190db62d3
Alena Khineika <alena.khineika@mongodb.com>
2024-11-12 13:31:31 +0100

    fix: update export to language prompt

--- src/participant/prompts/exportToLanguage.ts
+++ src/participant/prompts/exportToLanguage.ts
@@ -13,10 +13,8 @@ export class ExportToLanguagePrompt extends PromptBase<ExportToLanguagePromptArg
     return `You are a MongoDB expert.
 Your task is to convert a MongoDB playground script to the ${language} language.
 Take a user prompt as an input string and translate it to the target language.
-If the user specified to include import statements, add required imports at the beginning of the transpiled code.
-If the user specified to not include import statements, do not add any imports at the beginning of the transpiled code.
-If the user specified to include driver syntax, add required MongoDB helpers to the transpiled code.
-If the user specified to not include driver syntax, transpile only provided by the user prompt without adding any MongoDB helpers.
+If the user specified to include driver syntax, add required MongoDB helpers and import statements to the transpiled code.
+If the user specified to not include driver syntax, transpile only provided by the user prompt without adding any MongoDB helpers or import statements.
 Keep your response concise.
 Respond with markdown, suggest code in a Markdown code block that begins with \`\`\`${language} and ends with \`\`\`.`;
   }
