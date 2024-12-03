8c9347efae79c4113de114cf5b48714061f900ec
gagik <me@gagik.co>
2024-12-03 16:26:19 +0100

    remove playground shortcut

--- src/templates/playgroundFromDatabaseTreeItemTemplate.ts
+++ src/templates/playgroundFromDatabaseTreeItemTemplate.ts
@@ -3,7 +3,6 @@ import { createTemplate } from './templateHelpers';
 export const playgroundFromDatabaseTreeItemTemplate = createTemplate(
   (currentDatabase) => `// MongoDB Playground
 // Use Ctrl+Space inside a snippet or a string literal to trigger completions.
-// Use PLAYGROUND_SHORTCUT to run the playground
 
 // The current database to use.
 use(${currentDatabase});
