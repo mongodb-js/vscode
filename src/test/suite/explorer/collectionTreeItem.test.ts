44ba206d4ccd33ba8f2183340e65461cf1980bad
Tihomir Culig <tihomir.culig@mongodb.com>
2025-12-18 15:34:15 +0100

    Update collectionTreeItem.test.ts

--- src/test/suite/explorer/collectionTreeItem.test.ts
+++ src/test/suite/explorer/collectionTreeItem.test.ts
@@ -1,5 +1,6 @@
 import assert from 'assert';
 import sinon from 'sinon';
+import { afterEach } from 'mocha';
 import type { DataService } from 'mongodb-data-service';
 
 import CollectionTreeItem from '../../../explorer/collectionTreeItem';
