478f89ff851ca03b6e44328cdf4abe4b97707db1
Tihomir Culig <tihomirculig@gmail.com>
2026-01-19 13:58:47 +0100

    move app.tsx to root

--- src/views/app.tsx
+++ src/views/app.tsx
@@ -1,14 +1,14 @@
 import React, { lazy, Suspense } from 'react';
 import { LeafyGreenProvider } from '@mongodb-js/compass-components';
 import { VscodeProgressRing } from '@vscode-elements/react-elements';
-import { useDetectVsCodeDarkMode } from './use-detect-vscode-dark-mode';
-import type { WebviewType } from '../../utils/webviewHelpers';
+import { useDetectVsCodeDarkMode } from './webview-app/use-detect-vscode-dark-mode';
+import type { WebviewType } from '../utils/webviewHelpers';
 
 // Use lazy loading to avoid loading both vscode-api modules at once.
 // Each vscode-api.ts calls acquireVsCodeApi() at module load time,
 // and acquireVsCodeApi() can only be called once per webview.
-const OverviewPage = lazy(() => import('./overview-page'));
-const PreviewPage = lazy(() => import('../data-browsing-app/preview-page'));
+const OverviewPage = lazy(() => import('./webview-app/overview-page'));
+const PreviewPage = lazy(() => import('./data-browsing-app/preview-page'));
 
 declare global {
   interface Window {
