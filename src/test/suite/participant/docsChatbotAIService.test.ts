802b84783febe560ad3927c313c9589d81333a60
Gagik Amaryan <me@gagik.co>
2026-03-02 10:40:40 +0100

    chore: use the hook

--- src/test/suite/participant/docsChatbotAIService.test.ts
+++ src/test/suite/participant/docsChatbotAIService.test.ts
@@ -5,27 +5,28 @@ import sinon from 'sinon';
 import { DocsChatbotAIService } from '../../../participant/docsChatbotAIService';
 
 suite('DocsChatbotAIService Test Suite', function () {
-  let docsChatbotAIService: DocsChatbotAIService;
+  let service: DocsChatbotAIService;
+  let originalEnv: typeof process.env;
 
-  beforeEach(() => {
-    docsChatbotAIService = new DocsChatbotAIService();
+  beforeEach(function () {
+    originalEnv = process.env;
+    service = new DocsChatbotAIService();
   });
 
   afterEach(function () {
     sinon.restore();
+    process.env = originalEnv;
   });
 
   test('respects MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE env variable', function () {
     process.env.MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE =
       'https://custom.mongodb.com';
-    const service = new DocsChatbotAIService();
     expect(service._serverBaseUri).to.equal('https://custom.mongodb.com');
     delete process.env.MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE;
   });
 
   test('uses default base URI when override is not set', function () {
     delete process.env.MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE;
-    const service = new DocsChatbotAIService();
     expect(service._serverBaseUri).to.equal('https://knowledge.mongodb.com');
   });
 });
