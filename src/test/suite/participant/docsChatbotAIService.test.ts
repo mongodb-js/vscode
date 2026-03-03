c02438c370fe1c58889a71fcb9d5a733f6fba91f
Gagik Amaryan <me@gagik.co>
2026-03-03 09:59:44 +0100

    chore: fixup environment variables

--- src/test/suite/participant/docsChatbotAIService.test.ts
+++ src/test/suite/participant/docsChatbotAIService.test.ts
@@ -5,12 +5,10 @@ import sinon from 'sinon';
 import { DocsChatbotAIService } from '../../../participant/docsChatbotAIService';
 
 suite('DocsChatbotAIService Test Suite', function () {
-  let service: DocsChatbotAIService;
   let originalEnv: typeof process.env;
 
   beforeEach(function () {
-    originalEnv = process.env;
-    service = new DocsChatbotAIService();
+    originalEnv = { ...process.env };
   });
 
   afterEach(function () {
@@ -21,12 +19,13 @@ suite('DocsChatbotAIService Test Suite', function () {
   test('respects MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE env variable', function () {
     process.env.MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE =
       'https://custom.mongodb.com';
+    const service = new DocsChatbotAIService();
     expect(service._serverBaseUri).to.equal('https://custom.mongodb.com');
-    delete process.env.MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE;
   });
 
   test('uses default base URI when override is not set', function () {
     delete process.env.MONGODB_DOCS_CHATBOT_BASE_URI_OVERRIDE;
+    const service = new DocsChatbotAIService();
     expect(service._serverBaseUri).to.equal('https://knowledge.mongodb.com');
   });
 });
