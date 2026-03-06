a17f1466b2dc30a75df18e20cda4a97c728d7ed4
Le Roux Bodenstein <lerouxb@gmail.com>
2026-03-06 18:06:36 +0000

    fix: safely serialize everything we send across process boundaries VSCODE-762 (#1273)

--- src/views/dataBrowsingController.ts
+++ src/views/dataBrowsingController.ts
@@ -44,6 +44,7 @@ import {
   DataBrowserDocumentDeletedTelemetryEvent,
   DataBrowserClosedTelemetryEvent,
 } from '../telemetry';
+import { serializeBSON } from '../language/serializer';
 
 const log = createLogger('data browsing controller');
 
@@ -799,6 +800,7 @@ export default class DataBrowsingController {
     if (!connectionOptions) {
       throw new Error('No connection options found');
     }
+
     // TODO(VSCODE-757, VSCODE-758): connect only once and reuse the connection
     // for subsequent requests instead of reconnecting every time. Or switch to
     // only using node service provider in the whole extension, then use the
@@ -991,7 +993,7 @@ export default class DataBrowsingController {
       webview: panel.webview,
       databaseName: options.databaseName,
       collectionName: options.collectionName,
-      query: JSON.stringify(options.query),
+      query: serializeBSON(options.query),
     });
 
     panel.webview.onDidReceiveMessage(
