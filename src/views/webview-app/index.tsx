0c1b1f22d349299db33b9b12402bb8a0cf7ba23b
Alena Khineika <alena.khineika@mongodb.com>
2026-05-02 22:55:19 +0200

    chore(deps): bump some dependencies

--- src/views/webview-app/index.tsx
+++ src/views/webview-app/index.tsx
@@ -4,5 +4,5 @@ import { createRoot } from 'react-dom/client';
 import App from '../app';
 
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = createRoot(container as HTMLElement);
 root.render(<App />);
